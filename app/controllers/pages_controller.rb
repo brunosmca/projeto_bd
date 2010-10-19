3a87362b3af84de0fe905c78063d962df864cd7a
BRUNO ANDRADE <brunosmca@gmail.com>
2010-10-19 08:56:18 -0300

    changing tittle

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,5 @@ class PagesController < ApplicationController
   def home
     @title = "Home"
   end
-
-  def contact
-    @title = "Contact"
-  end
-  
-  def about
-    @title = "About"
-  end
-  
-  def help
-    @tile = "Help"
-  end
   
 end
